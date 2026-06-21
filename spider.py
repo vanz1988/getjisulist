bb4fab17dbb3705f8681d528a0a452c01b85caa4
vanz1988 <70456458+vanz1988@users.noreply.github.com>
2026-06-21 16:35:02 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -8,7 +8,6 @@ import re
 import requests
 import undetected_chromedriver as uc
 from datetime import datetime, timezone, timedelta
-from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
